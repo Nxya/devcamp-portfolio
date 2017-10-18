d2c2192bd07c767e65951ec534d62c1d8e3c6104
Alex <alexb3030@gmail.com>
2017-10-18 11:30:27 -0600

    Forgot a save

--- db/seeds.rb
+++ db/seeds.rb
@@ -49,8 +49,7 @@ puts "9 portfolio items created"
 
 3.times do |technology|
   Portfolio.last.technologies.create!(
-    name: "Technology #{technology}",
-    portfolio_id: Portfolio.last.id
+    name: "Technology #{technology}"
   )
 end
 
